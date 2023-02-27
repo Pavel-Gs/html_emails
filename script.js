9a1aee73b0dce790c2fefc3dda8ba9aa94aa65bb
Pavel-Gs <pavelgen@gmail.com>
2023-02-27 05:43:40 -0800

    restyled

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let counter = 0;	/*count amount of chosen elements*/
+let counter = 0;	/*count the amount of saved form inputs*/
 /*hide tooltips*/
 const hideTooltips = () => {
 	/*document.getElementsByTagName('h2')[0].removeAttribute("class");*/
@@ -8,54 +8,6 @@ const hideTooltips = () => {
 	};
 	const tooltipButtonElement = document.getElementById('hide-tooltip-button');
 	tooltipButtonElement.remove();
-};
-/*remove logo 2*/
-const removeIsaakLogo = () => {
-	counter += 1;
-	console.log(counter);
-	const isaakLogoElement = document.getElementById('isaak-logo');
-	isaakLogoElement.remove();
-	const geolinkButtonElement = document.getElementById('geolink-button');
-	geolinkButtonElement.remove();
-	const isaakButtonElement = document.getElementById('isaak-button');
-	isaakButtonElement.remove();
-	const isaakLinkElement = document.getElementById('isaak-link');
-	isaakLinkElement.remove();
-};
-/*remove logo 1*/
-const removeGeolinkLogo = () => {
-	counter += 1;
-	console.log(counter);
-	const geolinkLogoElement = document.getElementById('geolink-logo');
-	geolinkLogoElement.remove();
-	const isaakButtonElement = document.getElementById('isaak-button');
-	isaakButtonElement.remove();
-	const geolinkButtonElement = document.getElementById('geolink-button');
-	geolinkButtonElement.remove();
-	const geolinkLinkElement = document.getElementById('geolink-link');
-	geolinkLinkElement.remove();
-};
-/*remove header 2*/
-const removeWorkOrder = () => {
-	counter += 1;
-	console.log(counter);
-	const workOrderElement = document.getElementById('work-order-header');
-	workOrderElement.remove();
-	const workOrderButtonElement = document.getElementById('work-order-button');
-	workOrderButtonElement.remove();
-	const estimateButtonElement = document.getElementById('estimate-button');
-	estimateButtonElement.remove();
-};
-/*remove header 1*/
-const removeEstimate = () => {
-	counter += 1;
-	console.log(counter);
-	const estimateElement = document.getElementById('estimate-header');
-	estimateElement.remove();
-	const workOrderButtonElement = document.getElementById('work-order-button');
-	workOrderButtonElement.remove();
-	const estimateButtonElement = document.getElementById('estimate-button');
-	estimateButtonElement.remove();
 };
 /*save input 1*/
 const saveClientName = () => {
@@ -91,7 +43,9 @@ const saveClientAddress = () => {
 const saveClientProject = () => {
 	counter += 1;
 	console.log(counter);
-	document.getElementById('client-project').innerHTML = "Project description: " + document.getElementById('client-project-input').value;
+	document.getElementById('client-project').innerHTML = document.getElementById('client-project-input').value;
+	const clientProjectStarElement = document.getElementById('project-required-field');
+	clientProjectStarElement.remove();
 	const clientProjectInputElement = document.getElementById('client-project-input');
 	clientProjectInputElement.remove();
 	const clientProjectButtonElement = document.getElementById('client-project-button');
@@ -101,7 +55,9 @@ const saveClientProject = () => {
 const saveClientEstimate = () => {
 	counter += 1;
 	console.log(counter);
-	document.getElementById('client-estimate').innerHTML = "Estimate approximately: $" + document.getElementById('client-estimate-input').value + " plus GST.";
+	document.getElementById('client-estimate').innerHTML = document.getElementById('client-estimate-input').value + " plus GST.";
+	const clientEstimateStarElement = document.getElementById('estimate-required-field');
+	clientEstimateStarElement.remove();
 	const clientEstimateInputElement = document.getElementById('client-estimate-input');
 	clientEstimateInputElement.remove();
 	const clientEstimateButtonElement = document.getElementById('client-estimate-button');
