392d93f576103beb201dea9d68e16c35726ce9b9
Pavel-Gs <pavelgen@gmail.com>
2023-03-01 01:42:21 -0800

    keep working on JS and email CSS

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-let counter = 0;	/* count the amount of saved form inputs */
-
-/* hide tooltips */
+/* HIDE TOOLTIPS */
 const hideTooltips = () => {
 	const tips = document.querySelectorAll('.tooltip');
 	for (i = 0; i < tips.length; i++) {
 		tips[i].classList.remove('tooltip');
 	};
-	const tooltipButtonElement = document.getElementById('hide-tooltip-button');
-	tooltipButtonElement.remove();
+	document.getElementById('hide-tooltip-button').remove();
 };
 
-/* fill the form function */
-const saveElements = () => {
+/* FILL THE FORM, DELETE THE ELEMENTS AND INVOKE THE SEND FUNCTION */
+const fillAndSend = () => {
 	let inputValue1 = document.getElementById('client-name-input').value;
 	let inputValue2 = document.getElementById('company-name-input').value;
 	let inputValue3 = document.getElementById('client-address-input').value;
@@ -20,61 +17,47 @@ const saveElements = () => {
 	let inputValue6 = document.getElementById('client-estimate-input').value;
 
 	if (inputValue1 && inputValue2 && inputValue3 && inputValue5 && inputValue6) {
+		document.getElementById('web-wrapper').remove();
+		document.getElementById('memo-form').remove();
 
-		counter += 1;
 		document.getElementById('client-name').innerHTML = inputValue1;
-		const clientNameInputElement = document.getElementById('client-name-input');
-		clientNameInputElement.remove();
+		document.getElementById('client-name-input').remove();
 
-		counter += 1;
 		document.getElementById('client-company').innerHTML = "(" + inputValue2 + ")";
-		const companyNameInputElement = document.getElementById('company-name-input');
-		companyNameInputElement.remove();
+		document.getElementById('company-name-input').remove();
 
-		counter += 1;
 		document.getElementById('client-address').innerHTML = "Address: " + inputValue3;
-		const clientAddressInputElement = document.getElementById('client-address-input');
-		clientAddressInputElement.remove();
+		document.getElementById('client-address-input').remove();
 
-		counter += 1;
 		document.getElementById('client-email').innerHTML = inputValue4;
-		const clientEmailInputElement = document.getElementById('client-email-input');
-		clientEmailInputElement.remove();
+		document.getElementById('client-email-input').remove();
 
-		counter += 1;
 		document.getElementById('client-project').innerHTML = inputValue5;
-		const clientProjectStarElement = document.getElementById('project-required-field');
-		clientProjectStarElement.remove();
-		const clientProjectInputElement = document.getElementById('client-project-input');
-		clientProjectInputElement.remove();
+		document.getElementById('project-required-field').remove();
+		document.getElementById('client-project-input').remove();
 
-		counter += 1;
-		document.getElementById('client-estimate').innerHTML = inputValue6 + " plus GST.";
-		const clientEstimateStarElement = document.getElementById('estimate-required-field');
-		clientEstimateStarElement.remove();
-		const clientEstimateInputElement = document.getElementById('client-estimate-input');
-		clientEstimateInputElement.remove();
-	};
+		document.getElementById('client-estimate').innerHTML = inputValue6;
+		document.getElementById('estimate-required-field').remove();
+		document.getElementById('client-estimate-input').remove();
+
+		/* send email function (self-invoking) */
+		(function sendEmail() {
+			let inputValue = document.getElementById('client-email').innerHTML;
+			if (inputValue) {
+				document.getElementById('client-email').remove();
+				Email.send({
+					SecureToken: '9410fea9-caec-4800-9d73-faa1c7a96f66',
+					To: inputValue, // in order to send to multiple email addresses, make an array[]
+					From: "testing.websoft@gmail.com",
+					Subject: "Estimate / Work order",
+					Body: document.getElementById('email-body').innerHTML,
+				}).then(message => alert("email has been sent"));
+			};
+		})();
+	} else { window.alert("Please fill in ALL required fields."); };
 };
 
-/* send email function */
-function sendEmail() {
-	let inputValue = document.getElementById('client-email').innerHTML;
-	if (counter >= 6 && inputValue) {								// the required amount of fields that a user should fill and save
-		const clientEmailElement = document.getElementById('client-email');
-		clientEmailElement.remove();
-		Email.send({
-			SecureToken: '9410fea9-caec-4800-9d73-faa1c7a96f66',
-			To: inputValue,											// in order to send to multiple email addresses, make an array[]
-			From: "testing.websoft@gmail.com",
-			Subject: "Estimate / Work order",
-			Body: document.getElementById('email-body').innerHTML,
-		}).then(
-			message => alert("email has been sent"));
-	} else { window.alert("Please fill and save the required fields."); };
-};
-
-/* prevent page refresh on submit */
+/* PREVENT PAGE REFRESH ON SUBMIT */
 const memoForm = document.getElementById('memo-form');
 const preventPageRefresh = (event) => {
 	event.preventDefault();
